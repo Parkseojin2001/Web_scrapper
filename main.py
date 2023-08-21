a8c448cb185ce5b76d1c99a7b698f2085771fc56
Parkseojin2001 <seojin0510612@gmail.com>
2023-08-21 22:55:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from extractors.indeed import extract_indeed_jobs
-from extractors.wwr import extract_wwr_jobs
-from file import save_to_file
+from flask import Flask, render_template
 
-keyword = input("What do you want to search for?")
+app = Flask("JobScrapper")
 
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-jobs = indeed + wwr
+@app.route("/")
+def home():
+    return render_template("home.html", name = "nico")
 
-save_to_file(keyword, jobs)
-
-
+@app.route("/search")
+def hello():
+    return render_template("search.html")
+app.run("127.0.0.1")
+    
 
 
 
